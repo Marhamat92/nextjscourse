af651d4180bf99d101fbff51ad4b31f66920d4ab
Marhamat <75548742+Marhamat92@users.noreply.github.com>
2021-11-06 19:25:46 +0300

    some new features and routes added

---
+++ .history/pages/portfolio/list_20211106191145.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function listPage() {
+    return (
+        <div>
+            <h1>The List Page under portfolio page</h1>
+        </div>
+    )
+}
+
+export default listPage
